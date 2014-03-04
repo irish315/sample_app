d2e343802ce0091060bfcb9916c1bfa5abf7e890
irish315 <irish315cuse@gmail.com>
2014-03-04 11:27:46 -0500

    Add Page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  get "pages/Home"
+  get "pages/Contacts"
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
